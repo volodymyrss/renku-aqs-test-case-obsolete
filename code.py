fb9d1309788bf125581a4f7dc78bfae1e8fcb637
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-06-11 09:01:30 +0200

    code

--- code.py
+++ code.py
@@ -2,6 +2,9 @@
 import astroquery.legacysurvey
 
 #astroquery.legacysurvey.LegacySurvey.query_object_async(SkyCoord(83,22, unit="deg"))
+
+print(astroquery.utils.process_asyncs._original_async_to_sync)
+
 r = astroquery.legacysurvey.LegacySurvey.query_object_async("Crab")
 
 print("astroquery returns:", r)
