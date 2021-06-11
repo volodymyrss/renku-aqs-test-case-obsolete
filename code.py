9cc5437ed71726ba7299aa844cb65ea1967da2df
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-06-11 09:04:52 +0200

    code

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ import astroquery.legacysurvey
 
 print(astroquery.utils.process_asyncs._original_async_to_sync)
 
-r = astroquery.legacysurvey.LegacySurvey.query_region("Crab")
+r = astroquery.legacysurvey.LegacySurvey.query_object("nothing")
 
 print("astroquery returns:", r)
 
