526b56250af8b2bb650070b902c682815887b540
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-06-11 09:07:37 +0200

    code

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import astroquery.legacysurvey
 #astroquery.legacysurvey.LegacySurvey.query_object_async(SkyCoord(83,22, unit="deg"))
 
 print(astroquery.utils.process_asyncs._original_async_to_sync)
+print(astroquery.utils.process_asyncs.async_to_sync)
 
 r = astroquery.legacysurvey.LegacySurvey.query_object("nothing")
 
