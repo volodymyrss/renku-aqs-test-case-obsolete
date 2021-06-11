7ef4fd77f9399a51eb8feb1f9602ba3661c27ac2
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-06-11 08:49:51 +0200

    code

--- code.py
+++ code.py
@@ -1,3 +1,7 @@
-import astroquery
+from astropy.coordinates import SkyCoord
+import astroquery.legacysurvey
+
+astroquery.legacysurvey.LegacySurvey.query_region(SkyCoord(83,22, unit="deg"))
 
 open("test-output.txt", "w").write("test!" + str(astroquery.hooked))
+
